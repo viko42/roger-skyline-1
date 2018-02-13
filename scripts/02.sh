234b03a078cb11aef6f5a89fdc83559c60efed80
Viko@raspberry <victor@stationf.co>
2018-02-13 03:21:33 +0100

    scripts

--- scripts/02.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-apt-get update -y >> /var/log/update_script.log && apt-get upgrade -y >> /var/log/update_script.log
-echo "0 4 * * 1 /bin/sh /${PWD##*/}/02.sh" > cronfile
-crontab -u root cronfile
